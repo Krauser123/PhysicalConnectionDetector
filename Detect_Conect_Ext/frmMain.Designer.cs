6e21973a2b789e519591038b1838c817c04d3cb0
Krauser123 <66463181+Krauser123@users.noreply.github.com>
2020-07-27 11:42:37 +0200

    Add project files.

---
+++ Detect_Conect_Ext/frmMain.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace physicalConnectionDetector
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // txtLog
+            // 
+            this.txtLog.Location = new System.Drawing.Point(0, 0);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.ReadOnly = true;
+            this.txtLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtLog.Size = new System.Drawing.Size(401, 326);
+            this.txtLog.TabIndex = 0;
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.BalloonTipText = "Physical Connection Detector";
+            this.notifyIcon.BalloonTipTitle = "Physical Connection Detector";
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Text = "Physical Connection Detector";
+            this.notifyIcon.Visible = true;
+            this.notifyIcon.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.NotifyIcon_MouseDoubleClick);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(400, 323);
+            this.Controls.Add(this.txtLog);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FormMain";
+            this.Text = "Physical Connection Detector";
+            this.Resize += new System.EventHandler(this.FrmMain_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtLog;
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+    }
+}
+
---
+++ Detect_Conect_Ext/frmMain.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace physicalConnectionDetector
+{
+    public partial class FormMain : Form
+    {
+        readonly char[] units = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        //Data Structure that stores the handle of connections
+        [StructLayout(LayoutKind.Sequential)]
+        public struct DEV_BROADCAST_VOLUME
+        {
+            public int dbcv_size;
+            public int dbcv_devicetype;
+            public int dbcv_reserved;
+            public int dbcv_unitmask;
+        }
+
+        /// <summary>
+        /// Overides method to manage the arrival of new disk drives
+        /// </summary>
+        /// <param name="m"></param>
+        protected override void WndProc(ref Message m)
+        {
+            // These definitions are in dbt.h and winuser.h
+
+            // A change has occurred in the devices
+            const int WM_DEVICECHANGE = 0x0219;
+            // The system detects a new device
+            const int DBT_DEVICEARRIVAL = 0x8000;
+            // Request removal of the device
+            const int DBT_DEVICEQUERYREMOVE = 0x8001;
+            // Device removal failed
+            const int DBT_DEVICEQUERYREMOVEFAILED = 0x8002;
+            // Pending device removal
+            const int DBT_DEVICEREMOVEPENDING = 0x8003;
+            // Device removed from the system
+            const int DBT_DEVICEREMOVECOMPLETE = 0x8004;
+            // Logical volume (A disk has been inserted)
+            const int DBT_DEVTYP_VOLUME = 0x00000002;
+
+            string logToAdd = string.Empty;
+
+            switch (m.Msg)
+            {
+                // Change system devices
+                case WM_DEVICECHANGE:
+                    switch (m.WParam.ToInt32())
+                    {
+                        //Device connected
+                        case DBT_DEVICEARRIVAL:
+                            {
+                                int devType = Marshal.ReadInt32(m.LParam, 4);
+                                //It's a logical volume
+                                if (devType == DBT_DEVTYP_VOLUME)
+                                {
+                                    DEV_BROADCAST_VOLUME vol;
+                                    vol = (DEV_BROADCAST_VOLUME)Marshal.PtrToStructure(m.LParam, typeof(DEV_BROADCAST_VOLUME));
+
+                                    logToAdd = string.Format("{0} - {1} - Disk drive inserted: {2}", DateTime.Now.ToShortDateString(), DateTime.Now.ToShortTimeString(), UnitTag(vol.dbcv_unitmask));
+                                    txtLog.AppendText(logToAdd + Environment.NewLine);
+                                }
+                            }
+                            break;
+                        case DBT_DEVICEREMOVECOMPLETE:
+                            logToAdd = string.Format("{0} - {1} - Device removed.", DateTime.Now.ToShortDateString(), DateTime.Now.ToShortTimeString());
+                            txtLog.AppendText(logToAdd + Environment.NewLine);
+                            break;
+                    }
+                    break;
+            }
+
+            // Now use the default handler
+            base.WndProc(ref m);
+        }
+
+        /// <summary>
+        /// Detect the tag asigned to the unit
+        /// </summary>
+        /// <param name="unitmask"></param>
+        /// <returns></returns>
+        private char UnitTag(int unitmask)
+        {
+            int i = 0;
+
+            // We convert the mask into a primary array and look for the index of the first occurrence (the drive letter)
+            BitArray bitArrayMask = new BitArray(System.BitConverter.GetBytes(unitmask));
+            foreach (bool item in bitArrayMask)
+            {
+                if (item == true)
+                {
+                    break;
+                }
+                i++;
+            }
+            return units[i];
+        }
+
+        private void FrmMain_Resize(object sender, System.EventArgs e)
+        {
+            //When the form is minimized we want hide in system Tray represented by the NotifyIcon control
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                this.Hide();
+                notifyIcon.Visible = true;
+            }
+        }
+
+        private void NotifyIcon_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Show();
+            this.WindowState = FormWindowState.Normal;
+            notifyIcon.Visible = false;
+        }
+    }
+}
